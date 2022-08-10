2e0b2df032d18ff88a03c50e59dd837ace7f7a8d
“olaholunuga” <olaholunuga@gmail.com>
2022-08-10 01:33:00 +0100

    async

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,7 +6,7 @@ from typing import Coroutine
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Coroutine:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """fuuction to return an
     asyncronous task
     """
