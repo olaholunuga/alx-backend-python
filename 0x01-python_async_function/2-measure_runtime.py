a1e38c1a3b0b809511891cc3d6bb4c38f94ff8b4
“olaholunuga” <olaholunuga@gmail.com>
2022-08-10 01:28:48 +0100

    measure

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,10 +7,10 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """to measure time lapse of corutine
     """
     s = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     total_time = time.time() - s
     return total_time / n
