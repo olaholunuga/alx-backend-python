ce113d41391c8d3d97f148716b296f197ac8a663
“olaholunuga” <olaholunuga@gmail.com>
2022-08-10 01:06:11 +0100

    comprehensin

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """python async function
 """
-from typing import List, Union
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[Union[int, float]]:
+async def async_comprehension() -> List[float]:
     """function to return async comprehension
     """
     return [i async for i in async_generator()]
